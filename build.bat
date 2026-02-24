f84bd5840bf2317e0174e2829f53eff358d1406a
Raúl Ibáñez <raulibaper@gmail.com>
2026-02-24 09:00:29 +0100

    Bugfix: librería PIL ausente

--- build.bat
+++ build.bat
@@ -7,7 +7,24 @@ echo.
 call pip install pyinstaller
 echo.
 
-python -m PyInstaller --onefile --windowed --name "FirmaPDF" FirmaPDF.py
+python -m PyInstaller ^
+    --onefile ^
+    --windowed ^
+    --noupx ^
+    --name "FirmaPDF" ^
+    --exclude-module torch ^
+    --exclude-module tensorflow ^
+    --exclude-module keras ^
+    --exclude-module numpy ^
+    --exclude-module scipy ^
+    --exclude-module pandas ^
+    --exclude-module matplotlib ^
+    --exclude-module cv2 ^
+    --exclude-module sklearn ^
+    --exclude-module transformers ^
+    --exclude-module accelerate ^
+    --exclude-module sentencepiece ^
+    FirmaPDF.py
 
 echo.
 echo ============================================
